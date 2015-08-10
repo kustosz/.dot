6463c4e3e3beb8ee51f79f9fa32995044fe81a18
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2015-08-10 18:37:49 +0200

    cleanup vim config

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@ ln -s ~/.dot/zsh/zshrc ~/.zshrc
 ln -s ~/.dot/zsh ~/.zsh
 touch ~/.zshrc.local
 ln -s ~/.dot/gitignore ~/.gitignore
+
+git submodule init
+git submodule update
+mkdir vim/backup
+ln -s ~/.dot/vim/vimrc ~/.vimrc
+ln -s ~/.dot/vim ~/.vim
+vim +PluginInstall +qall
